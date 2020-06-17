31b2fcd3e1d0c9c0d7f9ba6c3a7ccfef338d888c
Sonya Gould <sonyavgould@gmail.com>
2020-06-17 00:17:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
+      binding.pry
       joined_strings << src[row_index][element_index] + " "
       
       element_index += 1
